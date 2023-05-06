1ee8319a0d77e5177cfddd0d96b12464d8afe610
João Gabriel Araújo Almeida <xxoodxm@gmail.com>
2023-05-06 16:46:00 -0300

    Atualização dos testes
    Criação dos testes das classes DAO que faltavam: Administrador, Cliente, Limpeza, ComponentePC, ComponenteOutro, Instalacao, Montagem, Tecnico, Recepcionista

---
+++ src/main/java/com/pbl/sistema_gerenciamento/utils/ManipulaArquivo.java
@@ -0,0 +1,30 @@
+package com.pbl.sistema_gerenciamento.utils;
+
+import java.util.List;
+
+public class ManipulaArquivo {
+    private String nome;
+
+    /**
+     *
+     */
+    public ManipulaArquivo(String nome){
+        this.nome = nome;
+    }
+
+    /**
+     *
+     */
+    public void guardar(){
+
+    }
+
+    /**
+     *
+     * @return
+     * @param <T>
+     */
+    public <T> List<T> retorna(){
+
+    }
+}
---
+++ src/test/java/com/pbl/sistema_gerenciamento/dao/ordemservico/ComponentePcListImplTest.java
@@ -0,0 +1,4 @@
+package com.pbl.sistema_gerenciamento.dao.ordemservico;
+
+public class ComponentePcListImplTest {
+}
