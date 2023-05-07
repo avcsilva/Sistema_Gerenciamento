2ea139ade7164b077c86783332c7c8853520145b
avcsilva <avcsilva@ecomp.uefs.br>
2023-05-07 18:41:39 -0300

    feat: adiciona teste para Instalação
    Adiciona teste para método "adicionarPrograma" da classe model Instalação.
    
    Co-Authored-By: João Gabriel Araújo Almeida  <112009180+joaogabrielaraujo@users.noreply.github.com>

--- src/test/java/com/pbl/sistema_gerenciamento/model/InstalacaoTest.java
+++ src/test/java/com/pbl/sistema_gerenciamento/model/InstalacaoTest.java
@@ -1,4 +1,23 @@
 package com.pbl.sistema_gerenciamento.model;
 
-public class InstalacaoTest {
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InstalacaoTest {
+
+    @Test
+    void adicionarPrograma() {
+        Instalacao inst = new Instalacao(0.0);
+        ArrayList<String> lista = new ArrayList<String>();
+        lista.add("IntelliJ");
+        lista.add("QuartusII");
+        inst.setProgramas(lista);
+        assertEquals(2, inst.getListaProgramas().size());
+
+        inst.adicionarPrograma("VSCode");
+        assertEquals(3, inst.getListaProgramas().size());
+    }
 }
