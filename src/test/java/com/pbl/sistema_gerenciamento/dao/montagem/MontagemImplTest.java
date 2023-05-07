25509b855504c131613245f9c7feb7c00008e3b6
João Gabriel Araújo Almeida <xxoodxm@gmail.com>
2023-05-07 10:50:29 -0300

    feat: criação dos testes
    criação dos testes da classe do tipo DAO referente a montagem

--- src/test/java/com/pbl/sistema_gerenciamento/dao/montagem/MontagemImplTest.java
+++ src/test/java/com/pbl/sistema_gerenciamento/dao/montagem/MontagemImplTest.java
@@ -1,8 +1,15 @@
 package com.pbl.sistema_gerenciamento.dao.montagem;
 
+import com.pbl.sistema_gerenciamento.dao.DAO;
+import com.pbl.sistema_gerenciamento.dao.ordemservico.OrdemServicoDAO;
+import com.pbl.sistema_gerenciamento.model.Cliente;
+import com.pbl.sistema_gerenciamento.model.Montagem;
+import com.pbl.sistema_gerenciamento.model.OrdemServico;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -10,22 +17,57 @@ class MontagemImplTest {
 
     @BeforeEach
     void setUp() {
+
     }
 
     @AfterEach
     void tearDown() {
+        DAO.getMontagemDAO().deletarTodos();
     }
 
     @Test
     void criar() {
+        MontagemDAO dao = DAO.getMontagemDAO();
+
+        Montagem montagem1 = new Montagem(0.0, 2.0);
+        Montagem montagem2 = new Montagem(0.0, 3.0);
+
+        montagem1.setId(0);
+        montagem1.setId(1);
+
+        Montagem novo1 = dao.criar(montagem1);
+        Montagem novo2 = dao.criar(montagem2);
+
+        assertEquals(novo1, montagem1);
+        assertEquals(novo2, montagem2);
+
+
     }
 
     @Test
     void acharPorId() {
+        MontagemDAO dao = DAO.getMontagemDAO();
+
+        dao.criar(new Montagem(0.0, 2.0));
+        dao.criar(new Montagem(0.0, 2.0));
+
+        Montagem montagem = dao.acharPorId(1);
+        assertEquals(1, montagem.getId());
+
+        montagem = dao.acharPorId(7);
+        assertNull(montagem);
     }
 
     @Test
     void acharTodos() {
+        MontagemDAO dao = DAO.getMontagemDAO();
+
+        dao.criar(new Montagem(0.0, 2.0));
+        dao.criar(new Montagem(0.0, 3.0));
+
+        List<Montagem> lista = dao.acharTodos();
+
+        assertEquals(2, lista.size());
     }
 
     @Test
@@ -34,17 +76,75 @@ class MontagemImplTest {
 
     @Test
     void deletar() {
+        MontagemDAO dao = DAO.getMontagemDAO();
+
+        dao.criar(new Montagem(0.0, 2.0));
+        dao.criar(new Montagem(0.0, 2.0));
+
+        dao.deletar(1);
+
+        assertNull(dao.acharPorId(1));
+
+        List<Montagem> lista = dao.acharTodos();
+
+        assertEquals(1, lista.size());
     }
 
     @Test
     void deletarTodos() {
+        MontagemDAO dao = DAO.getMontagemDAO();
+
+        dao.criar(new Montagem(00.00, 20.00));
+        dao.criar(new Montagem(00.00, 20.00));
+
+        dao.deletarTodos();
+
+        List<Montagem> lista = dao.acharTodos();
+
+        assertEquals(0, lista.size());
     }
 
     @Test
     void acharPorPreco() {
+        MontagemDAO dao = DAO.getMontagemDAO();
+
+
+        Montagem montagem1 = new Montagem(null, null);
+        Montagem montagem2 = new Montagem(null, null);
+        Montagem montagem3 = new Montagem(null, null);
+
+        montagem1.setPreco(12.25);
+        montagem2.setPreco(10.00);
+        montagem3.setPreco(12.25);
+
+        dao.criar(montagem1);
+        dao.criar(montagem2);
+        dao.criar(montagem3);
+
+        List<Montagem> lista = dao.acharPorPreco(12.25);
+
+        assertEquals(2, lista.size());
     }
 
     @Test
     void acharPorCusto() {
+        MontagemDAO dao = DAO.getMontagemDAO();
+
+
+        Montagem montagem1 = new Montagem(null, null);
+        Montagem montagem2 = new Montagem(null, null);
+        Montagem montagem3 = new Montagem(null, null);
+
+        montagem1.setCusto(12.25);
+        montagem2.setCusto(10.00);
+        montagem3.setCusto(12.25);
+
+        dao.criar(montagem1);
+        dao.criar(montagem2);
+        dao.criar(montagem3);
+
+        List<Montagem> lista = dao.acharPorPreco(12.25);
+
+        assertEquals(2, lista.size());
     }
 }
