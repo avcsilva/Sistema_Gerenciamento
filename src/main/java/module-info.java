1e0ed325281fd7f1c61d2fa0c2d0f71a0f8dade4
Rafael Tosta <rafa.ecomp@gmail.com>
2023-05-11 15:34:48 -0300

    :sparkles: feat: Suite de testes
    Testa todos módulos de um única vez.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -10,6 +10,18 @@ module com.pbl.sistema_gerenciamento {
     requires org.kordamp.bootstrapfx.core;
     requires com.almasb.fxgl.all;
 
+    opens com.pbl.sistema_gerenciamento.dao.montagem;
+    opens com.pbl.sistema_gerenciamento.dao.instalacao;
+    opens com.pbl.sistema_gerenciamento.dao.administrador;
+    opens com.pbl.sistema_gerenciamento.dao.cliente;
+    opens com.pbl.sistema_gerenciamento.dao.componenteoutro;
+    opens com.pbl.sistema_gerenciamento.dao.limpeza;
+    opens com.pbl.sistema_gerenciamento.dao.recepcionista;
+    opens com.pbl.sistema_gerenciamento.dao.tecnico;
+    opens com.pbl.sistema_gerenciamento.dao.ordemservico;
+    opens com.pbl.sistema_gerenciamento.dao.componentepc;
+    opens com.pbl.sistema_gerenciamento.model;
+
     opens com.pbl.sistema_gerenciamento to javafx.fxml;
     exports com.pbl.sistema_gerenciamento;
 }
