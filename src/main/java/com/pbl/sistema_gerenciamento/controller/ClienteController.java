233cd76ba3fa2e9851ec1a23077ebb163dce128d
avcsilva <avcsilva@ecomp.uefs.br>
2023-06-04 21:52:41 -0300

    feat: adiciona funcionalidades para Controller
    Adiciona funcionalidade para ação do botão de criar
    
    Co-Authored-By: João Gabriel Araújo Almeida  <112009180+joaogabrielaraujo@users.noreply.github.com>

--- src/main/java/com/pbl/sistema_gerenciamento/controller/ClienteController.java
+++ src/main/java/com/pbl/sistema_gerenciamento/controller/ClienteController.java
@@ -1,11 +1,12 @@
 package com.pbl.sistema_gerenciamento.controller;
 
+import com.pbl.sistema_gerenciamento.dao.DAO;
+import com.pbl.sistema_gerenciamento.model.Cliente;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 
 public class ClienteController {
 
@@ -34,30 +35,67 @@ public class ClienteController {
     private Label erro_msg;
 
     @FXML
-    private TableColumn<?, ?> tabelaEmails;
+    private TableView<Cliente> tabelaClientes;
 
     @FXML
-    private TableColumn<?, ?> tabelaEnderecos;
+    private TableColumn<Cliente, String> tabelaEmails;
 
     @FXML
-    private TableColumn<?, ?> tabelaNomes;
+    private TableColumn<Cliente, String> tabelaEnderecos;
 
     @FXML
-    private TableColumn<?, ?> tabelaTelefones;
+    private TableColumn<Cliente, String> tabelaNomes;
 
+    @FXML
+    private TableColumn<Cliente, String> tabelaTelefones;
+
+    private ObservableList<Cliente> clientesLista;
+
+    /**
+     * Apaga o cliente selecionado
+     * @param event
+     */
     @FXML
     void btnApgAction(ActionEvent event) {
 
     }
 
+    /**
+     * Atualiza os dados do cliente selecionado
+     * @param event
+     */
     @FXML
     void btnAttAction(ActionEvent event) {
 
     }
 
+    /**
+     * Cria um novo cliente
+     * @param event
+     */
     @FXML
     void btnCriaAction(ActionEvent event) {
+        if (clienteNome.getText().isEmpty() || clienteEndereco.getText().isEmpty() || clienteTelefone.getText().isEmpty() || clienteEmail.getText().isEmpty()) {
+            this.erro_msg.setText("Preencha todos os campos!");
+        } else {
+            Cliente cliente = new Cliente(clienteNome.getText(), clienteEndereco.getText(), clienteTelefone.getText(), clienteEmail.getText());
+            DAO.getClienteDAO().criar(cliente);
+            this.erro_msg.setText("Cliente criado com sucesso!");
+            this.clienteEmail.clear();
+            this.clienteEndereco.clear();
+            this.clienteNome.clear();
+            this.clienteTelefone.clear();
+        }
+    }
 
+    /**
+     * Inicializa a tela
+     */
+    @FXML
+    void initialize() {
+        this.clientesLista = FXCollections.observableArrayList(DAO.getClienteDAO().acharTodos());
+        this.tabelaClientes.getColumns().addAll(tabelaNomes, tabelaEnderecos, tabelaTelefones, tabelaEmails);
+        this.tabelaClientes.setItems(clientesLista);
     }
 
 }
