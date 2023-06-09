30f91bac1a8ad9309f12fab42e1235c4f4f77dfb
avcsilva <avcsilva@ecomp.uefs.br>
2023-06-09 19:47:04 -0300

    Update InstalacaoController.java
    Co-Authored-By: João Gabriel Araújo Almeida  <112009180+joaogabrielaraujo@users.noreply.github.com>

--- src/main/java/com/pbl/sistema_gerenciamento/controller/InstalacaoController.java
+++ src/main/java/com/pbl/sistema_gerenciamento/controller/InstalacaoController.java
@@ -1,5 +1,9 @@
 package com.pbl.sistema_gerenciamento.controller;
 
+import com.pbl.sistema_gerenciamento.dao.DAO;
+import com.pbl.sistema_gerenciamento.model.Instalacao;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -7,6 +11,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class InstalacaoController {
 
@@ -32,30 +37,52 @@ public class InstalacaoController {
     private TextField instalacaoSO;
 
     @FXML
-    private TableView<?> tabelaInstals;
+    private TableView<Instalacao> tabelaInstals;
 
     @FXML
-    private TableColumn<?, ?> tabelaPrecos;
+    private TableColumn<Instalacao, Double> tabelaPrecos;
 
     @FXML
-    private TableColumn<?, ?> tabelaProgramas;
+    private TableColumn<Instalacao, String> tabelaProgramas;
 
     @FXML
-    private TableColumn<?, ?> tabelaSO;
+    private TableColumn<Instalacao, String> tabelaSO;
+
+    private ObservableList<Instalacao> instalacaoList;
 
     @FXML
     void btnApgAction(ActionEvent event) {
-
+        Instalacao instalacao = this.tabelaInstals.getSelectionModel().getSelectedItem();
+        if(instalacao ==  null){
+        this.erro_msg.setText("Instalação não selecionada");
+        }else{
+            this.instalacaoList.remove(instalacao);
+            DAO.getInstalacaoDAO().deletar(instalacao.getId());
+            this.erro_msg.setText("Instação removida com sucesso");
+        }
     }
 
     @FXML
     void btnAttAction(ActionEvent event) {
+        Inst
+
 
     }
 
     @FXML
     void btnCriaAction(ActionEvent event) {
 
+    }
+
+    @FXML
+    void initialize(){
+        this.instalacaoList = FXCollections.observableArrayList(DAO.getInstalacaoDAO().acharTodos());
+
+        this.tabelaPrecos.setCellValueFactory(new PropertyValueFactory<Instalacao, Double>("preco"));
+        this.tabelaProgramas.setCellValueFactory(new PropertyValueFactory<Instalacao, String>("programa"));
+        this.tabelaSO.setCellValueFactory(new PropertyValueFactory<Instalacao, String>("sistemaOperacional"));
+
+        this.tabelaInstals.setItems(this.instalacaoList);
     }
 
 }
