9a1c7c836a4f81f798d9761e8c5a74e9097643d4
avcsilva <avcsilva@ecomp.uefs.br>
2023-06-09 20:45:02 -0300

    Create LoginController.java
    Co-Authored-By: João Gabriel Araújo Almeida  <112009180+joaogabrielaraujo@users.noreply.github.com>

---
+++ src/main/java/com/pbl/sistema_gerenciamento/controller/LoginController.java
@@ -0,0 +1,28 @@
+package com.pbl.sistema_gerenciamento.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+
+    @FXML
+    private Button btncontinuar;
+
+    @FXML
+    private Label erro;
+
+    @FXML
+    private TextField loginBox;
+
+    @FXML
+    private TextField senhaBox;
+
+    @FXML
+    void btncontinuarAction(ActionEvent event) {
+
+    }
+
+}
