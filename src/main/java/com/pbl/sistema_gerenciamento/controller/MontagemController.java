d26143ee62ef5456829b79f60954ca2697c75fcb
avcsilva <avcsilva@ecomp.uefs.br>
2023-06-09 22:49:02 -0300

    feat: implementa controller Montagem
    Faz as implementações do controller para a janela de criação e gerenciamento de Montagem.
    
    Co-Authored-By: João Gabriel Araújo Almeida  <112009180+joaogabrielaraujo@users.noreply.github.com>

--- src/main/java/com/pbl/sistema_gerenciamento/controller/MontagemController.java
+++ src/main/java/com/pbl/sistema_gerenciamento/controller/MontagemController.java
@@ -1,10 +1,16 @@
 package com.pbl.sistema_gerenciamento.controller;
 
+import com.pbl.sistema_gerenciamento.dao.DAO;
+import com.pbl.sistema_gerenciamento.model.Montagem;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class MontagemController {
 
@@ -12,27 +18,32 @@ public class MontagemController {
     private Button btnApg;
 
     @FXML
-    private Button btnAtt;
-
-    @FXML
     private Button btnCriar;
 
     @FXML
-    private TableColumn<?, ?> tabelaCustos;
+    private Label erro_msg;
 
     @FXML
-    private TableView<?> tabelaMontagens;
+    private TableColumn<Montagem, Double> tabelaCustos;
 
     @FXML
-    private TableColumn<?, ?> tabelaPrecos;
+    private TableView<Montagem> tabelaMontagens;
+
+    @FXML
+    private TableColumn<Montagem, Double> tabelaPrecos;
+
+    private ObservableList<Montagem> montagemList;
 
     @FXML
     void btnApgAction(ActionEvent event) {
-
-    }
-
-    @FXML
-    void btnAttAction(ActionEvent event) {
+        Montagem montagem = this.tabelaMontagens.getSelectionModel().getSelectedItem();
+        if(montagem ==  null){
+            this.erro_msg.setText("Montagem não selecionada");
+        }else{
+            this.montagemList.remove(montagem);
+            DAO.getInstalacaoDAO().deletar(montagem.getId());
+            this.erro_msg.setText("Montagem removida com sucesso");
+        }
 
     }
 
@@ -41,4 +52,13 @@ public class MontagemController {
 
     }
 
+    @FXML
+    void initialize(){
+        this.montagemList = FXCollections.observableArrayList();
+        
+          this.tabelaPrecos.setCellValueFactory(new PropertyValueFactory<Montagem, Double>("preco"));
+          this.tabelaCustos.setCellValueFactory(new PropertyValueFactory<Montagem, Double>("custo"));
+
+          this.tabelaMontagens.setItems(this.montagemList);
+    }
 }
