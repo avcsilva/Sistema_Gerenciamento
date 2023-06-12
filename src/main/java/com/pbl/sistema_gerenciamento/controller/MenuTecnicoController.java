4942b82d874f87bb7c2de4a72efa62da4fe670c2
avcsilva <avcsilva@ecomp.uefs.br>
2023-06-12 01:17:48 -0300

    Update MenuTecnicoController.java
    Implementa botões para o menu
    
    Co-Authored-By: João Gabriel Araújo Almeida  <112009180+joaogabrielaraujo@users.noreply.github.com>

--- src/main/java/com/pbl/sistema_gerenciamento/controller/MenuTecnicoController.java
+++ src/main/java/com/pbl/sistema_gerenciamento/controller/MenuTecnicoController.java
@@ -1,10 +1,18 @@
 package com.pbl.sistema_gerenciamento.controller;
 
+import com.pbl.sistema_gerenciamento.HelloApplication;
 import com.pbl.sistema_gerenciamento.model.Tecnico;
+import com.pbl.sistema_gerenciamento.utils.StageController;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MenuTecnicoController {
 
@@ -45,47 +53,161 @@ public class MenuTecnicoController {
 
     @FXML
     void btnFaturaAction(ActionEvent event) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("JanelaFinalizaOrdem.fxml"));
+            Scene scene = new Scene(fxmlLoader.load());
 
+            Stage stage = new Stage();
+            stage.setScene(scene);
+            stage.setTitle("Finalização de ordem de serviço");
+            stage.setResizable(false);
+            stage.centerOnScreen();
+            stage.initModality(Modality.APPLICATION_MODAL);
+
+            FinalizaOrdemController controller = fxmlLoader.getController();
+            controller.setTecnico(this.tecnico);
+
+            stage.showAndWait();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @FXML
     void btnGereClieAction(ActionEvent event) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("JanelaCliente.fxml"));
+            Scene scene = new Scene(fxmlLoader.load());
 
+            Stage stage = new Stage();
+            stage.setScene(scene);
+            stage.setTitle("Registro de cliente");
+            stage.setResizable(false);
+            stage.centerOnScreen();
+            stage.initModality(Modality.APPLICATION_MODAL);
+
+            stage.showAndWait();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @FXML
     void btnGereCompcAction(ActionEvent event) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("JanelaComponentePC.fxml"));
+            Scene scene = new Scene(fxmlLoader.load());
 
+            Stage stage = new Stage();
+            stage.setScene(scene);
+            stage.setTitle("Registro de componente PC");
+            stage.setResizable(false);
+            stage.centerOnScreen();
+            stage.initModality(Modality.APPLICATION_MODAL);
+
+            stage.showAndWait();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @FXML
     void btnGereCompoutAction(ActionEvent event) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("JanelaComponenteOutro.fxml"));
+            Scene scene = new Scene(fxmlLoader.load());
 
+            Stage stage = new Stage();
+            stage.setScene(scene);
+            stage.setTitle("Registro de componente outro");
+            stage.setResizable(false);
+            stage.centerOnScreen();
+            stage.initModality(Modality.APPLICATION_MODAL);
+
+            stage.showAndWait();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @FXML
     void btnGereInstAction(ActionEvent event) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("JanelaInstalacao.fxml"));
+            Scene scene = new Scene(fxmlLoader.load());
 
+            Stage stage = new Stage();
+            stage.setScene(scene);
+            stage.setTitle("Registro de instalação");
+            stage.setResizable(false);
+            stage.centerOnScreen();
+            stage.initModality(Modality.APPLICATION_MODAL);
+
+            stage.showAndWait();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @FXML
     void btnGereLimpAction(ActionEvent event) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("JanelaLimpeza.fxml"));
+            Scene scene = new Scene(fxmlLoader.load());
 
+            Stage stage = new Stage();
+            stage.setScene(scene);
+            stage.setTitle("Registro de limpeza");
+            stage.setResizable(false);
+            stage.centerOnScreen();
+            stage.initModality(Modality.APPLICATION_MODAL);
+
+            stage.showAndWait();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @FXML
     void btnGereMontAction(ActionEvent event) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("JanelaMontagem"));
+            Scene scene = new Scene(fxmlLoader.load());
 
+            Stage stage = new Stage();
+            stage.setScene(scene);
+            stage.setTitle("Registro de montagem");
+            stage.setResizable(false);
+            stage.centerOnScreen();
+            stage.initModality(Modality.APPLICATION_MODAL);
+
+            stage.showAndWait();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @FXML
     void btnGereOrdemAction(ActionEvent event) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("JanelaOrdemServico.fxml"));
+            Scene scene = new Scene(fxmlLoader.load());
 
+            Stage stage = new Stage();
+            stage.setScene(scene);
+            stage.setTitle("Registro de ordem de serviço");
+            stage.setResizable(false);
+            stage.centerOnScreen();
+            stage.initModality(Modality.APPLICATION_MODAL);
+
+            stage.showAndWait();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @FXML
     void btnPegaOrdAction(ActionEvent event) {
-
     }
 
     public void setTecnico(Tecnico tecnico) {
