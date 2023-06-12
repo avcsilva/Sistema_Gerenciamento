c826c86838f3c94ec246958bb5c056777e283e2c
avcsilva <avcsilva@ecomp.uefs.br>
2023-06-11 23:58:18 -0300

    feat: implementa controller
    Implementa funcionalidade para botão de confirma e configura inicialização da tela

--- src/main/java/com/pbl/sistema_gerenciamento/controller/LoginController.java
+++ src/main/java/com/pbl/sistema_gerenciamento/controller/LoginController.java
@@ -2,10 +2,15 @@ package com.pbl.sistema_gerenciamento.controller;
 
 import com.pbl.sistema_gerenciamento.HelloApplication;
 import com.pbl.sistema_gerenciamento.utils.StageController;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
@@ -14,19 +19,25 @@ import java.io.IOException;
 public class LoginController {
 
     @FXML
+    private Button btnConfirma;
+
+    @FXML
+    private Label erro_msg;
+
+    @FXML
     private TextField loginBox;
 
     @FXML
     private TextField senhaBox;
 
     @FXML
-    private ComboBox<?> tipoUsuario;
+    private ComboBox<String> tipoUsuario;
 
     @FXML
-    void initialize(){
+    void btnConfirmaAction(ActionEvent event) {
         if (loginBox.getText().isEmpty() && senhaBox.getText().isEmpty()) {
-            this.erro.setText("Login e senha não podem ser vazios");
-            this.erro.setStyle("-fx-text-fill: red;");
+            this.erro_msg.setText("Login e senha não podem ser vazios");
+            this.erro_msg.setStyle("-fx-text-fill: red;");
         } else {
             try{
                 FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("ALGUMAJANELAQUIPORFAVOR.fxml"));
@@ -38,5 +49,11 @@ public class LoginController {
                 e.printStackTrace();
             }
         }
+    }
+
+    @FXML
+    void initialize(){
+        ObservableList<String> tipos = FXCollections.observableArrayList("Administrador","Técnico", "Recepcionista");
+        this.tipoUsuario.setItems(tipos);
     }
 }
