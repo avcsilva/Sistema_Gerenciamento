3940503dbf515f577953121df0cb362992e75177
avcsilva <avcsilva@ecomp.uefs.br>
2023-06-12 02:33:49 -0300

    Update HelloApplication.java
    Co-Authored-By: João Gabriel Araújo Almeida  <112009180+joaogabrielaraujo@users.noreply.github.com>

--- src/main/java/com/pbl/sistema_gerenciamento/HelloApplication.java
+++ src/main/java/com/pbl/sistema_gerenciamento/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(getClass().getResource("JanelaCliente.fxml"));
+        loader.setLocation(getClass().getResource("JanelaLogin.fxml"));
         Scene scene = new Scene(loader.load());
         stage.setScene(scene);
         stage.setResizable(false);
