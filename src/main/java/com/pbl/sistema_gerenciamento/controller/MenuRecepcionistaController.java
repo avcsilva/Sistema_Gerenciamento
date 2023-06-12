990b03f2498a1eefc7851f34f93d8087bfcc74c9
avcsilva <avcsilva@ecomp.uefs.br>
2023-06-12 02:32:34 -0300

    Update MenuRecepcionistaController.java
    Co-Authored-By: João Gabriel Araújo Almeida  <112009180+joaogabrielaraujo@users.noreply.github.com>

--- src/main/java/com/pbl/sistema_gerenciamento/controller/MenuRecepcionistaController.java
+++ src/main/java/com/pbl/sistema_gerenciamento/controller/MenuRecepcionistaController.java
@@ -1,9 +1,16 @@
 package com.pbl.sistema_gerenciamento.controller;
 
+import com.pbl.sistema_gerenciamento.HelloApplication;
 import com.pbl.sistema_gerenciamento.model.Recepcionista;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MenuRecepcionistaController {
 
@@ -26,27 +33,97 @@ public class MenuRecepcionistaController {
 
     @FXML
     void btnGereClieAction(ActionEvent event) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("JanelaCliente.fxml"));
+            Scene scene = new Scene(fxmlLoader.load());
 
+            Stage stage = new Stage();
+            stage.setScene(scene);
+            stage.setTitle("Registro de cliente");
+            stage.setResizable(true);
+            stage.centerOnScreen();
+            stage.initModality(Modality.APPLICATION_MODAL);
+
+            stage.showAndWait();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @FXML
     void btnGereInstAction(ActionEvent event) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("JanelaInstalacao.fxml"));
+            Scene scene = new Scene(fxmlLoader.load());
 
+            Stage stage = new Stage();
+            stage.setScene(scene);
+            stage.setTitle("Registro de instalação");
+            stage.setResizable(true);
+            stage.centerOnScreen();
+            stage.initModality(Modality.APPLICATION_MODAL);
+
+            stage.showAndWait();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @FXML
     void btnGereLimpAction(ActionEvent event) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("JanelaLimpeza.fxml"));
+            Scene scene = new Scene(fxmlLoader.load());
 
+            Stage stage = new Stage();
+            stage.setScene(scene);
+            stage.setTitle("Registro de limpeza");
+            stage.setResizable(true);
+            stage.centerOnScreen();
+            stage.initModality(Modality.APPLICATION_MODAL);
+
+            stage.showAndWait();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @FXML
     void btnGereMontAction(ActionEvent event) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("JanelaMontagem.fxml"));
+            Scene scene = new Scene(fxmlLoader.load());
 
+            Stage stage = new Stage();
+            stage.setScene(scene);
+            stage.setTitle("Registro de montagem");
+            stage.setResizable(true);
+            stage.centerOnScreen();
+            stage.initModality(Modality.APPLICATION_MODAL);
+
+            stage.showAndWait();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @FXML
     void btnGereOrdemAction(ActionEvent event) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("JanelaOrdemServico.fxml"));
+            Scene scene = new Scene(fxmlLoader.load());
 
+            Stage stage = new Stage();
+            stage.setScene(scene);
+            stage.setTitle("Registro de ordem de serviço");
+            stage.setResizable(true);
+            stage.centerOnScreen();
+            stage.initModality(Modality.APPLICATION_MODAL);
+
+            stage.showAndWait();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void setRecepcionista(Recepcionista recepcionista) {
