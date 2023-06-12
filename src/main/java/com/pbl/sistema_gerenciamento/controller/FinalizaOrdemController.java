faed06542aea92b9cd8bcf64482b078df0ee9264
avcsilva <avcsilva@ecomp.uefs.br>
2023-06-12 02:31:50 -0300

    Update FinalizaOrdemController.java
    Co-Authored-By: João Gabriel Araújo Almeida  <112009180+joaogabrielaraujo@users.noreply.github.com>

--- src/main/java/com/pbl/sistema_gerenciamento/controller/FinalizaOrdemController.java
+++ src/main/java/com/pbl/sistema_gerenciamento/controller/FinalizaOrdemController.java
@@ -1,5 +1,7 @@
 package com.pbl.sistema_gerenciamento.controller;
 
+import com.pbl.sistema_gerenciamento.dao.DAO;
+import com.pbl.sistema_gerenciamento.model.OrdemServico;
 import com.pbl.sistema_gerenciamento.model.Tecnico;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -9,6 +11,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class FinalizaOrdemController {
 
@@ -17,6 +20,9 @@ public class FinalizaOrdemController {
 
     @FXML
     private Button btnFinaliza;
+
+    @FXML
+    private Label erro_msg;
 
     @FXML
     private Label cliente;
@@ -41,23 +47,55 @@ public class FinalizaOrdemController {
 
     private Tecnico tecnico;
 
+    private Stage dialogStage;
+
+    public void setDialogStage(Stage dialogStage) {
+        this.dialogStage = dialogStage;
+    }
+
     public void setTecnico(Tecnico tecnico) {
         this.tecnico = tecnico;
     }
 
     @FXML
     void btnCancelaAction(ActionEvent event) {
+        OrdemServico ordem = this.tecnico.getOrdemAssociada();
+        ordem.setTecnico(null);
+        ordem.setStatus("cancelada");
+        DAO.getOrdemServicoDAO().atualizar(ordem);
+        this.tecnico.setOrdemAssociada(null);
+        DAO.getTecnicoDAO().atualizar(this.tecnico);
+        this.dialogStage.close();
     }
 
     @FXML
     void btnFinalizaAction(ActionEvent event) {
-
+        if (this.dataf.getText().isEmpty() || this.metPag.getSelectionModel().getSelectedItem() == null){
+            this.erro_msg.setText("Preencha todos os campos!");
+            this.erro_msg.setStyle("-fx-text-fill: red;");
+        } else{
+            OrdemServico ordem = this.tecnico.getOrdemAssociada();
+            ordem.setTecnico(null);
+            ordem.setStatus("finalizada");
+            ordem.setFinalizacao(this.dataf.getText());
+            ordem.setMetodoPagamento(this.metPag.getSelectionModel().getSelectedItem());
+            DAO.getOrdemServicoDAO().atualizar(ordem);
+            this.tecnico.setOrdemAssociada(null);
+            DAO.getTecnicoDAO().atualizar(this.tecnico);
+            this.dialogStage.close();
+        }
     }
 
     @FXML
     void initialize(){
         ObservableList<String> metodos = FXCollections.observableArrayList("Dinheiro", "Cartão de Crédito", "Cartão de Débito", "PIX");
         this.metPag.setItems(metodos);
-    }
 
+        this.id.setText(Integer.toString(this.tecnico.getOrdemAssociada().getId()));
+        this.cliente.setText(this.tecnico.getOrdemAssociada().getCliente().getNome());
+        this.datac.setText(this.tecnico.getOrdemAssociada().getCriacao());
+        this.custo.setText(Double.toString(this.tecnico.getOrdemAssociada().getCusto()));
+        this.preco.setText(Double.toString(this.tecnico.getOrdemAssociada().getPreco()));
+
+    }
 }
