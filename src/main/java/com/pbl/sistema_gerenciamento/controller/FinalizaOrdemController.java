4eff173705e05795fde2a4b9b86a359d5517104a
avcsilva <avcsilva@ecomp.uefs.br>
2023-06-12 01:17:56 -0300

    Update FinalizaOrdemController.java
    Co-Authored-By: João Gabriel Araújo Almeida  <112009180+joaogabrielaraujo@users.noreply.github.com>

--- src/main/java/com/pbl/sistema_gerenciamento/controller/FinalizaOrdemController.java
+++ src/main/java/com/pbl/sistema_gerenciamento/controller/FinalizaOrdemController.java
@@ -1,5 +1,8 @@
 package com.pbl.sistema_gerenciamento.controller;
 
+import com.pbl.sistema_gerenciamento.model.Tecnico;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -31,19 +34,30 @@ public class FinalizaOrdemController {
     private Label id;
 
     @FXML
-    private ChoiceBox<?> metPag;
+    private ChoiceBox<String> metPag;
 
     @FXML
     private Label preco;
 
+    private Tecnico tecnico;
+
+    public void setTecnico(Tecnico tecnico) {
+        this.tecnico = tecnico;
+    }
+
     @FXML
     void btnCancelaAction(ActionEvent event) {
-
     }
 
     @FXML
     void btnFinalizaAction(ActionEvent event) {
 
+    }
+
+    @FXML
+    void initialize(){
+        ObservableList<String> metodos = FXCollections.observableArrayList("Dinheiro", "Cartão de Crédito", "Cartão de Débito", "PIX");
+        this.metPag.setItems(metodos);
     }
 
 }
